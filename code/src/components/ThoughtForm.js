a3789712cfd922c34cec7cd24eab03ec4249d1b1
Idautterstrom <ida.utterstrom@hotmail.com>
2022-01-03 10:53:58 +0100

    fixed bug

--- code/src/components/ThoughtForm.js
+++ code/src/components/ThoughtForm.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./thoughtForm.css";
 
 const ThoughtForm = ({ onFormSubmit, newThought, setNewThought }) => {
-  //Props
   return (
     <form onSubmit={onFormSubmit} className="form-container">
       <label htmlFor="newThought" className="heading">
@@ -20,7 +19,13 @@ const ThoughtForm = ({ onFormSubmit, newThought, setNewThought }) => {
         type="submit"
         className="submit-button"
       >
-        &#10084;&#65039; Send Happy Thought &#10084;&#65039;
+        <span role="img" area-label="heart">
+          &#10084;&#65039;
+        </span>{" "}
+        Send Happy Thought{" "}
+        <span role="img" area-label="heart">
+          &#10084;&#65039;
+        </span>
       </button>
     </form>
   );
